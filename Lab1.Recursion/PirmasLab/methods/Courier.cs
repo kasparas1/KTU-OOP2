907b47f328d66a221e48ea3fa13bc8c0756f2a11
MajorTom <kasparask11@gmail.com>
2023-03-02 19:21:25 +0200

    for friend

---
+++ Lab1.Recursion/PirmasLab/methods/Courier.cs
@@ -0,0 +1,35 @@
+﻿public class Courier
+{
+    // Fields
+   
+
+    // Properties
+    public int Count { get; }
+    public int[,] Prices { get; }
+    private int[] Route;
+    // Indexer
+    public int this[int index]
+    {
+        get => Route[index];
+        set => Route[index] = value;
+    }
+    // Constructor
+    public Courier(int count, int[,] prices)
+    {
+        Count = count;
+        Prices = prices;
+        Route = FirstRoute(Count);
+    }
+    // Private method
+    private int[] FirstRoute(int count)
+    {
+        int[] points = new int[count + 1];
+        for (int i = 0; i < count; i++)
+        {
+            points[i] = i + 1;
+        }
+        points[count] = 1;
+
+        return points;
+    }
+}
